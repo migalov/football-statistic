e27fbddf65cedb0af3a8f623adf1d5cb65a5b579
Michel Muniagiseny <mafri.michel@gmail.com>
2023-08-29 15:37:30 +0300

    Using routing

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    Hello Nuxt
+    <NuxtLayout>
+      <header>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/events">Events</NuxtLink></li>
+        </ul>
+      </header>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Index param
+  </div>
+</template>
