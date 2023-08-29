9ca17e16746f9a2041190a722f42e0aceba2184c
Michel Muniagiseny <mafri.michel@gmail.com>
2023-08-29 17:32:49 +0300

    Adding a counter using useState()

---
+++ components/Counter.vue
@@ -0,0 +1,15 @@
+<script setup>
+  const counter = useCounter();
+</script>
+
+<template>
+  <div>
+  Counter: {{ counter }}
+  <button @click="counter++">
+    +
+  </button>
+  <button @click="counter--">
+    -
+  </button>
+</div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,28 @@
   // const { $sayHello } = useNuxtApp()
   // console.log(useNuxtApp());
   // $sayHello(`Michel Migalov`);
-  definePageMeta({
-    middleware: 'auth'
-  })
-
+  // definePageMeta({
+  //   middleware: 'auth'
+  // })
+  // const counter = useState('counter', () => Math.round(Math.random() * 1000))
+  const counter = useCounter();
 </script>
 
 <template>
-  <div>
-    <!-- <h1>Hi, man!</h1>
+  <!-- <div>
+    <h1>Hi, man!</h1>
     <img src="2.jpg" alt="" srcset="">
-    <IconsBell /> -->
+    <IconsBell />
     <ContentDoc />
+  </div> -->
+  <div id="main">
+    Counter: {{ counter }}
+    <button @click="counter++">
+      +
+    </button>
+    <button @click="counter--">
+      -
+    </button>
   </div>
+  <Counter id="counter" />
 </template>
