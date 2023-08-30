5b11c429cef6a7a4021f3178760f972b6ff4add5
Michel Muniagiseny <mafri.michel@gmail.com>
2023-08-30 16:35:24 +0300

    Using hook app:created

--- plugins/myPlugin.ts
+++ plugins/myPlugin.ts
@@ -1,7 +1,5 @@
 export default defineNuxtPlugin(nuxtApp => {
-  return {
-    provide: {
-      sayHello: (msg: string) => console.log(`Hello, ${msg}`)
-    }
-  }
+  nuxtApp.hook("app:created", () => {
+    console.log("Hello from plugin");
+  })
 })
